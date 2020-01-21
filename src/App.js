ba560cb44feec2f80da8364a723ae47e259b275d
Radoslav Georgiev <ninetensfive@gmail.com>
2020-01-21 17:36:03 +0200

    added shop page and items in it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import { Switch, Route } from 'react-router-dom';
+
 
 import './App.css';
-
-
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route />
+      </Switch>
     </div>
   );
 }
